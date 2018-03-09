e15cca6d3c6be4967527f701234b2e51ff3ecb06
倪述荣 <xueron@xueron.com>
2018-03-09 10:57:20 +0800

    optimize code

--- src/TraceTask.php
+++ src/TraceTask.php
@@ -10,7 +10,7 @@ use Uniondrug\Server\Task;
 
 class TraceTask extends Task\TaskHandler
 {
-    public function handle($data)
+    public function handle($data = [])
     {
         $traceClient = $this->traceClient;
         call_user_func_array([$traceClient, $traceClient->getMethod()], [$data]);
